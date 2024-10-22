fcc82e03a23b461b50b3d57b7ac1c45e73da51cb
Dipta saha <66532706+didipta@users.noreply.github.com>
2024-10-22 16:23:19 +0600

    model controller add

---
+++ lib/data/models/slider_list_model.dart
@@ -0,0 +1,19 @@
+
+import 'package:e_commerce_app/data/models/slider_model.dart';
+
+class SliderListModel {
+  String? msg;
+  List<SliderModel>? sliderList;
+
+  SliderListModel({this.msg, this.sliderList});
+
+  SliderListModel.fromJson(Map<String, dynamic> json) {
+    msg = json['msg'];
+    if (json['data'] != null) {
+      sliderList = <SliderModel>[];
+      json['data'].forEach((v) {
+        sliderList!.add(SliderModel.fromJson(v));
+      });
+    }
+  }
+}
